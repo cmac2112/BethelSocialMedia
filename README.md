d51fa03495857e90d25961a3d62f9ccb4e595045
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-10-08 18:56:57 -0500

    project initialization

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# testbranch
-sfgdfgdfgdfgdfgdfgdfg
-fsdfsdfdsfds
-peepeepoopooshidfart
+# Steps for starting this application
+
+1. Start the client - naviate to BethelSocialMedia\bethel-social-client> 
+
+- npm i
+- npm run dev
+
+2. Start the server
+
+- open a new terminal, navigate to BethelSocialMedia\bethel-social-server>
+
+- npm i
+- node index.js - to start the server
+- node db-init - to drop and repopulate the database
---
+++ bethel-social-server/index.js
@@ -0,0 +1,17 @@
+//require('dotenv').config()
+//const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
+
+app.get('/api/test', (req, res)=>{
+    console.log(req.method + ' request for ' + req.url);
+        res.send({message: 'test api'})
+    })
+
+const port = process.env.SERVER_PORT || 3000;
+    app.listen(port, () =>{
+        console.log('Server started on port ' + port);
+    })
