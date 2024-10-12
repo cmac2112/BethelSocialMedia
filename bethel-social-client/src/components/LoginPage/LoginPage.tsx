6ad8b296ec3df6220fb5d1b989c8f2d0ad84dfa5
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-10-11 19:28:56 -0500

    poject setup with tailwinds and routing

---
+++ bethel-social-client/src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginPage = () => {
+  return (
+    <div>LoginPage
+        <Link to="/home" className="p-5 text-blue-500">Go to Home Page</Link>
+        <Link to="/profile" className="p-5 text-blue-500">Go to Profile Page</Link>
+    </div>
+  )
+}
+
+export default LoginPage
