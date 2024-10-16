5e4233509c28f909e08736e0457ded93a363ffa1
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-10-16 14:00:11 -0500

    added parts of profile page

--- bethel-social-client/src/components/LoginPage/LoginPage.tsx
+++ bethel-social-client/src/components/LoginPage/LoginPage.tsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 const LoginPage = () => {
   return (
     <div>Landing page, make changes here
-        <Link to="/home" className="p-5 text-blue-500">Go to Home Page</Link>
-        <Link to="/profile" className="p-5 text-blue-500">Go to Profile Page</Link>
+        <Link to="/home" className="p-5 text-maroon">Go to Home Page</Link>
+        <Link to="/profile" className="p-5 text-maroon">Go to Profile Page</Link>
     </div>
   )
 }
--- bethel-social-client/src/components/ProfilePage/ProfilePage.tsx
+++ bethel-social-client/src/components/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../Layout'
+import pfp from "../../assets/testimage.jpg"
 
 const ProfilePage = () => {
+  const [user, setUser] = useState('test user')
+  //obviously change images later
   return (
     <Layout>
-      <div>ProfilePage</div>
+      <div className="container max-w-full flex justify-evenly bg-gray-50 relative" id="pfp-title-container">
+        <div className="p-10">
+          <img src={pfp} className="rounded-full h-28 w-28" />
+        </div>
+        <div className="p-10">
+        <h2 className="text-3xl font-semibold">Welcome,</h2>
+        <h2 className="text-end text-xl font-semibold">{user}</h2>
+        </div>
+        <div className="justify-self-end"id="settings">
+          <img src={pfp} className="rounded-full h-5 w-5 absolute top-2 right-2" />
+        </div>
+      </div>
     </Layout>
   )
 }
